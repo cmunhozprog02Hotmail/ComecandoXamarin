fba52efc16fd9a173bc3b0d8aab8ca92ce45b787
cmunhozprog02Hotmail <cmunhozprog02@hotmail.com>
2017-05-08 01:22:09 -0300

    Aula 02 - Criação do PCL

---
+++ ComecandoXamarin/ComecandoXaramin.PCL/IndiceMassaCorporal.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ComecandoXaramin.PCL
+{
+    public class IndiceMassaCorporal
+    {
+        public static decimal Calcular(decimal peso, decimal altura)
+        {
+            return peso * (altura * altura);
+        }
+    }
+}
